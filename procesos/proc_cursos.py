09ba9367461a3771d7a7719f7a6b0a3be20c5f56
Juan J. Rojas <48027894+juanjorojash@users.noreply.github.com>
2024-11-14 17:12:49 -0600

    codigos masomenos

--- procesos/proc_cursos.py
+++ procesos/proc_cursos.py
@@ -4,4 +4,4 @@ cursos = pd.read_csv("cursos.csv")
 
 cursos.insert(0,'id',cursos["area"] + cursos["semestre"].astype(str).str.zfill(2) + cursos["fila"].astype(str).str.zfill(2))
 
-cursos.to_csv('cursos.csv',index=False)
+cursos.to_csv('cursosid.csv',index=False)
