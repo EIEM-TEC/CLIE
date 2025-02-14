b1fa39adcdfe10cb68784650210f9cc7d03fdae7
Juan J. Rojas <48027894+juanjorojash@users.noreply.github.com>
2025-02-14 11:25:00 -0600

    nuevos reqs

--- procesos/proc_req.py
+++ procesos/proc_req.py
@@ -17,5 +17,17 @@ for index, row in cursosreq.iterrows():
         else:
             cursos.loc[cursos['id'] == req, 'esrequisito'] = cursos[cursos['id'] == req]['esrequisito'].item() + ';' + id
 
+cursoscorreq = cursos[cursos['correquisitos'].notna()]
+for index, row in cursoscorreq.iterrows():
+    id = row['id']
+    correqs = row['correquisitos'].split(';')
+    for correq in correqs:
+        print(correq)
+        curr_val = cursos.loc[cursos['id'] == correq, 'esrequisito'].item()
+        if curr_val == None:
+            cursos.loc[cursos['id'] == correq, 'esrequisito'] = id
+        else:
+            cursos.loc[cursos['id'] == correq, 'esrequisito'] = cursos[cursos['id'] == correq]['esrequisito'].item() + ';' + id
+
 
 cursos.to_csv('./cursos/cursos_malla.csv',index=False)
