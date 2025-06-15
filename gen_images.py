a67bf97f45ed0a28c52841151e5f9f2a6f5d2ab6
Juan J. Rojas <48027894+juanjorojash@users.noreply.github.com>
2025-06-15 14:02:29 -0600

    nuevas imagenes

--- gen_images.py
+++ gen_images.py
@@ -3,13 +3,13 @@ from pdf2image import convert_from_path
 pdf_path = "./malla_EM.pdf"
 
 
-images = convert_from_path(pdf_path, dpi=400, first_page=1, last_page=4)
+images = convert_from_path(pdf_path, dpi=900, first_page=1, last_page=4)
 
 # Save images as PNG files
 image_paths = []
 for i, image in enumerate(images):
-    image_path = f"./malla_{i+1}.jpg"
-    image.save(image_path, "JPEG")
+    image_path = f"./malla_{i+1}.png"
+    image.save(image_path, "PNG")
     image_paths.append(image_path)
 
 
