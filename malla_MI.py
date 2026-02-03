6514bb4304e20d86c1f8efd68c93d1fee1882970
Juan J. Rojas <juanjorojash@gmail.com>
2026-02-03 16:13:54 -0600

    mallaMI

---
+++ malla_MI.py
@@ -0,0 +1,152 @@
+import pandas as pd
+from pylatex import Document, Package, Command, PageStyle, Head, Foot, NewPage,\
+    TextColor, MiniPage, StandAloneGraphic, simple_page_number,\
+    TikZ, TikZScope, TikZNode, TikZOptions, TikZCoordinate, TikZNodeAnchor, TikZPath,\
+    UnsafeCommand,\
+    VerticalSpace, HorizontalSpace, NewLine,\
+    LongTable
+from pylatex.base_classes import Environment, Arguments
+from pylatex.utils import NoEscape, bold, italic
+import funciones as fun
+
+cursos = pd.read_csv("cursos/cursos_malla_MI.csv",
+    dtype = {'id':str,'codigo':str,'nombre':str,'area':str,'semestre':int,'fila':int,'horasTeoria':int,'horasPractica':int,'creditos':int})
+cursos['sevesreq'] = cursos['creditos'] * 0.0
+cursos['sevreq'] = cursos['creditos'] * 0.0
+
+
+areas = pd.read_csv("areas.csv")
+
+MI = ["BAS","SOL","ELE","GES","TER"]
+
+# black, blue, brown, cyan, darkgray, gray, green, lightgray, lime, magenta, olive, orange, pink, purple, red, teal, violet, white, yellow.
+
+area_colors = {
+    "BAS": "blue!20!white",
+    "SOL": "brown!20!white",
+    "ELE": "cyan!20!white",
+    "GES": "green!20!white",
+    "TER": "magenta!20!white",
+}
+
+def generar_malla():
+    #Geometría
+    geometry_options = { 
+        "left": "0mm",
+        "right": "0mm",
+        "top": "1mm",
+        "bottom": "0mm",
+        "headheight": "1mm",
+        "footskip": "1mm"
+    }
+    #Opciones del documento
+    doc = Document(documentclass="article", \
+                   fontenc=None, \
+                   inputenc=None, \
+                   lmodern=False, \
+                   textcomp=False, \
+                   page_numbers=True, \
+                   indent=False, \
+                   document_options=["letterpaper","landscape"],
+                   geometry_options=geometry_options)
+    #Paquetes
+    doc.packages.append(Package(name="fontspec", options=None))
+    doc.packages.append(Package(name="babel", options=['spanish',"activeacute"]))
+    doc.packages.append(Package(name="graphicx"))
+    doc.packages.append(Package(name="tikz"))
+    doc.packages.append(Package(name="anyfontsize"))
+    doc.packages.append(Package(name="xcolor",options="dvipsnames"))
+    doc.packages.append(Package(name="colortbl"))
+    doc.packages.append(Package(name="array"))
+    doc.packages.append(Package(name="float"))
+    doc.packages.append(Package(name="longtable"))
+    doc.packages.append(Package(name="multirow"))
+    doc.packages.append(Package(name="fancyhdr"))
+    doc.preamble.append(NoEscape(r'\usetikzlibrary{arrows.meta}'))
+    #Bloques
+    bloqueTitulo = NoEscape(
+    r'''\tikzset{
+            pics/titulo/.style args={#1,#2}{
+            code={
+                \def\ancho{75}
+                \def\alto{0.7}
+                \draw[fill=#2] (-\ancho/2+7,2*\alto) rectangle (\ancho/2+7,-2*\alto) node[midway,align=center,text width=45cm]{\fontsize{30pt}{0pt}\selectfont \textbf{#1}};
+            }
+        }
+    }'''
+    )
+    bloqueCurso = NoEscape(
+    r'''\tikzset{
+            pics/curso/.style args={#1,#2,#3,#4,#5,#6}{
+            code={
+                \def\ancho{5}
+                \def\alto{0.8}
+                \draw[fill=#6] (-\ancho/2,\alto) rectangle (\ancho/2,-\alto) node[midway,align=center,text width=\ancho cm]{\fontsize{16pt}{2pt}\selectfont {#2}};
+                \draw[fill=#6] (-\ancho/2,\alto) rectangle (\ancho/2,\alto + \alto) node[midway]{\fontsize{14pt}{14pt}\selectfont #1};
+                \draw[fill=#6] (-\ancho/2,-\alto) rectangle (-\ancho/2 + \ancho/3, -\alto - \alto) node[midway]{\fontsize{14pt}{14pt}\selectfont #3};
+                \draw[fill=#6] (-\ancho/2 + \ancho/3,-\alto) rectangle (-\ancho/2 + 2*\ancho/3, -\alto - \alto) node[midway]{\fontsize{14pt}{14pt}\selectfont #4};
+                \draw[fill=#6] (-\ancho/2 + 2*\ancho/3,-\alto) rectangle (-\ancho/2 + 3*\ancho/3, -\alto - \alto) node[midway]{\fontsize{14pt}{14pt}\selectfont #5};
+            }
+        }
+    }''' 
+    #1: codigo, #2: nombre, #3: horasteoria, #4: horaspractica, #5: creditos, #6: color
+    )
+    bloqueSemestre = NoEscape(
+    r'''\tikzset{
+            pics/semestre/.style args={#1,#2,#3,#4,#5}{
+            code={
+                \def\ancho{6}
+                \def\alto{0.8}
+                \draw[fill=#2] (-\ancho/2,1.5*\alto) rectangle (\ancho/2,-1.5*\alto) node[midway,align=center,text width=\ancho cm]{\fontsize{16pt}{12pt}\selectfont \textbf{#1}};
+                \draw[fill=#2] (-\ancho/2,-\alto) rectangle (-\ancho/2 + \ancho/3, -\alto - \alto) node[midway]{\fontsize{12pt}{14pt}\selectfont #3};
+                \draw[fill=#2] (-\ancho/2 + \ancho/3,-\alto) rectangle (-\ancho/2 + 2*\ancho/3, -\alto - \alto) node[midway]{\fontsize{12pt}{14pt}\selectfont #4};
+                \draw[fill=#2] (-\ancho/2 + 2*\ancho/3,-\alto) rectangle (-\ancho/2 + 3*\ancho/3, -\alto - \alto) node[midway]{\fontsize{12pt}{14pt}\selectfont #5};
+            }
+        }
+    }'''
+    #1: semestre, #2: color, #3: horasteoria, #4: horaspractica, #5: creditos
+    )
+    
+    doc.preamble.append(bloqueTitulo)        
+    doc.preamble.append(bloqueCurso)
+    doc.preamble.append(bloqueSemestre)
+
+    doc.append(Command('centering'))
+    sesgo = 0
+    rango = range(0,11)
+    with doc.create(TikZ(
+            options=TikZOptions
+                (    
+                "scale = 0.37",
+                "transform shape"
+                )
+        )) as malla_MI:
+        titulo = "Licenciatura en Ingeniería en Mantenimiento Industrial"
+        fun.malla_enf(malla_MI,cursos,sesgo,MI,"MI",area_colors,titulo,rango,False)
+    doc.generate_pdf(f"malla_MI", clean=True, clean_tex=False, compiler='lualatex',silent=True)
+    
+
+
+# datos_malla = pd.DataFrame()
+# cursos_malla = cursos[cursos["semestre"]<=10]
+# datos_malla["cred_TRC"] = cursos_malla.groupby("area")["creditos"].sum(numeric_only=True)
+# datos_malla.loc[["INS","AER","SCF"],"cred_TRC"] = 0
+# datos_malla["pt_TRC"] = areas["porcTRC"].to_list()[:-2]
+# datos_malla["pm_TRC"] = round((datos_malla["cred_TRC"] / datos_malla["cred_TRC"].sum())*100,1)
+# datos_malla["cred_INS"] = cursos_malla.groupby("area")["creditos"].sum(numeric_only=True)
+# datos_malla.loc[["AER","SCF"],"cred_INS"] = 0
+# datos_malla["pt_INS"] = areas["porcINS"].to_list()[:-2]
+# datos_malla["pm_INS"] = round(((datos_malla["cred_INS"]/180)*100),1)
+# datos_malla["cred_AER"] = cursos_malla.groupby("area")["creditos"].sum(numeric_only=True)
+# datos_malla.loc[["INS","SCF"],"cred_AER"] = 0
+# datos_malla["pt_AER"] = areas["porcAER"].to_list()[:-2]
+# datos_malla["pm_AER"] = round(((datos_malla["cred_AER"]/180)*100),1)
+# datos_malla["cred_SCF"] = cursos_malla.groupby("area")["creditos"].sum(numeric_only=True)
+# datos_malla.loc[["AER","INS"],"cred_SCF"] = 0
+# datos_malla["pt_SCF"] = areas["porcSCF"].to_list()[:-2]
+# datos_malla["pm_SCF"] = round(((datos_malla["cred_SCF"]/180)*100),1)
+# datos_malla.loc["TOT"] = datos_malla.sum(numeric_only=True)
+# datos_malla.to_csv('datos_malla_MI.csv')
+# print(datos_malla)
+
+generar_malla()
---
+++ procesos/proc_req_MI.py
@@ -0,0 +1,37 @@
+# Este archivo es para crear la columna esrequisito a partir de la columna requisitos de cada curso
+import pandas as pd
+
+cursos = pd.read_csv("./cursos/cursos_malla_MI.csv")
+
+cursos['esrequisito'] = None # borrar los datos de la columna
+
+cursosreq = cursos[cursos['requisitos'].notna()]
+for index, row in cursosreq.iterrows():
+    id = row['id']
+    print(f"\nCurso: {id}")
+    reqs = row['requisitos'].split(';')
+    for req in reqs:
+        print(f"Requisito: {req}")
+        curr_val = cursos.loc[cursos['id'] == req, 'esrequisito'].item()
+        if curr_val == None:
+            cursos.loc[cursos['id'] == req, 'esrequisito'] = id
+        else:
+            cursos.loc[cursos['id'] == req, 'esrequisito'] = cursos[cursos['id'] == req]['esrequisito'].item() + ';' + id
+
+cursoscorreq = cursos[cursos['correquisitos'].notna()]
+for index, row in cursoscorreq.iterrows():
+    id = row['id']
+    print(f"\nCurso: {id}")
+    correqs = row['correquisitos'].split(';')
+    for correq in correqs:
+        print(f"Correquisito: {correq}")
+        curr_val = cursos.loc[cursos['id'] == correq, 'esrequisito'].item()
+        if curr_val == None:
+            cursos.loc[cursos['id'] == correq, 'esrequisito'] = id
+        else:
+            cursos.loc[cursos['id'] == correq, 'esrequisito'] = cursos[cursos['id'] == correq]['esrequisito'].item() + ';' + id
+
+
+print(cursos.head(50))
+
+cursos.to_csv('./cursos/cursos_malla_MI.csv',index=False)
